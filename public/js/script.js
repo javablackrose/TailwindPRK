ab8aa9377ed4a142eaabe938d372689cc246a558
javablackrose <javablackrose@gmail.com>
2024-06-14 15:29:25 +0700

    setting navbar

--- public/js/script.js
+++ public/js/script.js
@@ -18,30 +18,3 @@ $('.carouselslick').slick({
     arrows: true,
     variableWidth: false,
 });
-
-// $('.carouselslick').slick({
-//     infinite: true,
-//     centerMode: true,
-//     centerPadding: '60px',
-//     slidesToShow: 3,
-//     responsive: [
-//         {
-//             breakpoint: 768,
-//             settings: {
-//                 arrows: false,
-//                 centerMode: true,
-//                 centerPadding: '40px',
-//                 slidesToShow: 3
-//             }
-//         },
-//         {
-//             breakpoint: 480,
-//             settings: {
-//                 arrows: false,
-//                 centerMode: true,
-//                 centerPadding: '40px',
-//                 slidesToShow: 1
-//             }
-//         }
-//     ]
-// });
